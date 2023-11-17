5875feec99b313f42dc071a7a2561403a60a1310
dilan-flores <dilan.flores@epn.edu.ec>
2023-11-17 15:45:58 -0500

    configuración cámara-mensaje

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
 export class Tab3Page {
 
   constructor() {}
+
+  enviarMensaje(){
+    window.alert("HOLAA..!!")
+  }
 
 }
